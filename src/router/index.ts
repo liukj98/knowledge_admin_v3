4b4171fa155d2bc191ba414dec2d60d67ab8d2d7
lkjisme <1795066394@qq.com>
2024-05-09 10:14:49 +0800

    去除登入模块

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,13 +37,13 @@ export const constantRoutes: RouteRecordRaw[] = [
     },
     alias: "/:pathMatch(.*)*"
   },
-  {
-    path: "/login",
-    component: () => import("@/views/login/index.vue"),
-    meta: {
-      hidden: true
-    }
-  },
+  // {
+  //   path: "/login",
+  //   component: () => import("@/views/login/index.vue"),
+  //   meta: {
+  //     hidden: true
+  //   }
+  // },
   {
     path: "/",
     component: Layouts,
